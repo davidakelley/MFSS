30fa3c69f1fa6b1cba4bbc9a31ac023ef093ad1b
David Kelley <dkelley@frbchi.org>
2017-01-30 16:04:06 -0600

    v1.3. Using custom diagnostic plots that persist between solvers.

--- StateSpaceEstimation.m
+++ StateSpaceEstimation.m
@@ -22,14 +22,15 @@ classdef StateSpaceEstimation < AbstractStateSpace
   
   properties
     % Screen output during ML estimation
-    verbose = true;
+    verbose = false;
+    diagnosticPlot = true;
     
     solver = {'fmincon', 'fminsearch'};
     solveIterMax = 20;
     
     % ML-estimation tolerances
     tol = 1e-10;      % Final estimation tolerance
-    stepTol = 1e-11;  % Step-size tolerance for ML theta elements
+    stepTol = 1e-8;  % Step-size tolerance for ML theta elements
     solveTol = 1e-11;   % EM tolerance
     
     % ML Estimation parameters
@@ -78,7 +79,7 @@ classdef StateSpaceEstimation < AbstractStateSpace
     end
     
     %% Estimation methods
-    function [ss_out, flag, gradient] = estimate(obj, y, ss0)
+    function [ss_out, flag, gradient, diagnostic] = estimate(obj, y, ss0)
       % Estimate missing parameter values via maximum likelihood.
       %
       % ss = ss.estimate(y, ss0) estimates any missing parameters in ss via
@@ -109,8 +110,6 @@ classdef StateSpaceEstimation < AbstractStateSpace
         displayType = 'none';
       end
       
-      plotFcnsCon = {@optimplotfval, @optimplotfirstorderopt, ...
-        @optimplotx, @optimplotconstrviolation};
       optFMinCon = optimoptions(@fmincon, ...
         'Algorithm', 'interior-point', ...
         'SpecifyObjectiveGradient', obj.useGrad, ...
@@ -120,8 +119,9 @@ classdef StateSpaceEstimation < AbstractStateSpace
         'FunctionTolerance', obj.tol, ...
         'OptimalityTolerance', obj.tol, ...
         'StepTolerance', obj.stepTol, ...
-        'PlotFcns', plotFcnsCon, ...
+        'OutputFcn', @outfun, ...
         'TolCon', 0);
+      
       optFMinUnc = optimoptions(@fminunc, ...
         'Algorithm', 'quasi-newton', ...
         'SpecifyObjectiveGradient', obj.useGrad, ...
@@ -131,20 +131,41 @@ classdef StateSpaceEstimation < AbstractStateSpace
         'FunctionTolerance', obj.tol, ...
         'OptimalityTolerance', obj.tol, ...
         'StepTolerance', obj.stepTol, ...
-        'PlotFcns', plotFcnsCon);
+        'OutputFcn', @outfun);
       
-      plotFcnsSearch = {@optimplotfval, @optimplotx};
       optFMinSearch = optimset('Display', displayType, ...
         'MaxFunEvals', 5000 * obj.ThetaMapping.nTheta, ...
-        'MaxIter', 500 * obj.ThetaMapping.nTheta, ...
-        'PlotFcns', plotFcnsSearch);
+        'MaxIter', 500, ...
+        'OutputFcn', @outfun);
       
       warning off MATLAB:nearlySingularMatrix;
       
       solverFun = obj.solver;
       
-      iter = 0; logli = []; logli0 = []; 
-      while iter < obj.solveIterMax && (iter < 2 || logli0 - logli > obj.solveTol)
+      % Save history of theta values
+      nTheta = size(theta0, 1);
+      if iscell(obj.solver)
+        maxPossibleIters = max(optFMinUnc.MaxIterations, optFMinSearch.MaxIter) * obj.solveIterMax;
+      else
+        switch obj.sovler
+          case {'fminunc', 'fmincon'}
+            maxPossibleIters = optFMinUnc.MaxIterations * obj.solveIterMax;
+          case 'fminsearch'
+            maxPossibleIters = optFMinSearch.MaxIter * obj.solveIterMax;
+        end
+      end
+      
+      if obj.diagnosticPlot
+        StateSpaceEstimation.initFigure(theta0);
+      end
+      
+      thetaHist = nan(maxPossibleIters, nTheta);
+      thetaIter = 0;
+      solverIter = nan(maxPossibleIters, 1);
+      
+      iter = 0; logli = []; logli0 = []; stopestim = false;
+      while ~stopestim && iter < obj.solveIterMax && ...
+          (iter < 2 || logli0 - logli > obj.solveTol)
         iter = iter + 1;
         logli0 = logli;
         if iscell(obj.solver)
@@ -180,6 +201,38 @@ classdef StateSpaceEstimation < AbstractStateSpace
   
       % Save estimated system to current object
       ss_out = obj.ThetaMapping.theta2system(thetaHat);
+      
+      % Create diagnostic output
+      thetaHist(thetaIter+1:end,:) = [];
+      solverIter(thetaIter+1:end) = [];
+      diagnostic = compileStruct(thetaHist, solverIter);
+      
+      % Nested function for getting diagnostic output
+      function stop = outfun(x,optimValues,state)
+        thetaIter = thetaIter + 1;
+        thetaHist(thetaIter, :) = x';
+        solverIter(thetaIter) = iter;
+        stop = false;
+        
+        if obj.diagnosticPlot
+          if strcmpi(getappdata(gcf, 'data'), 'stop')
+            stop = true;
+            setappdata(gcf, 'data', '');
+            state = 'done';
+          end
+          if strcmpi(getappdata(gcf, 'data'), 'stopestim')
+            stop = true;
+            stopestim = true;
+            state = 'done';
+          end
+          
+          optimValues.iteration = thetaIter;
+          
+          StateSpaceEstimation.sseplotpoint(x, optimValues, state);
+          StateSpaceEstimation.sseplotvalue(x, optimValues, state, iter);
+          drawnow;
+        end
+      end
     end
     
     function obj = em_estimate(obj, y, ss0, varargin)
@@ -306,7 +359,125 @@ classdef StateSpaceEstimation < AbstractStateSpace
         keyboard;
       end
     end
+  end
+  
+  methods (Static = true)
+    % Plot functions for ML estimation
+    function initFigure(theta0)
+      % Create diagnostic figure
+      screenSize = get(0, 'ScreenSize');
+      winSize = [725 700];
+      diagnosticF = figure('Color', ones(1,3), 'Position', ...
+        [screenSize(3)./2-0.5*winSize(1), screenSize(4)./2-0.5*winSize(2), winSize]);
+      diagnosticF.Position(3:4) = [725 700];
+      subplot(2, 1, 1);
+      StateSpaceEstimation.sseplotpoint(theta0, [], 'setup', 0);
+      subplot(2, 1, 2);
+      StateSpaceEstimation.sseplotvalue(theta0, [], 'setup', 0);
+      
+      % Give a stop button in the figure
+      stopBtnXYLoc = [2 5];
+      stopBtn = uicontrol('string', 'Stop Current Solver', ...
+        'Position',[stopBtnXYLoc 50 20],'callback',@buttonStop);
+      % Make sure the full text of the button is shown
+      stopBtnExtent = get(stopBtn,'Extent');
+      stopBtnPos = [stopBtnXYLoc stopBtnExtent(3:4)+[2 2]]; % Read text extent of stop button
+      % Set the position, using the initial hard coded position, if it is long enough
+      set(stopBtn,'Position',max(stopBtnPos,get(stopBtn,'Position')));
+      
+      function buttonStop(~,~)
+        setappdata(gcf,'data','stop');
+      end
+      
+      % Give a stop button in the figure
+      stopBtnXYLoc = [2+stopBtnExtent(3)+2 5];
+      stopEstimBtn = uicontrol('string', 'Stop Estimation', ...
+        'Position',[stopBtnXYLoc 50 20],'callback', @buttonStopEstim);
+      % Make sure the full text of the button is shown
+      stopBtnExtent = get(stopEstimBtn,'Extent');
+      stopBtnPos = [stopBtnXYLoc stopBtnExtent(3:4)+[2 2]]; % Read text extent of stop button
+      % Set the position, using the initial hard coded position, if it is long enough
+      set(stopEstimBtn,'Position',max(stopBtnPos,get(stopEstimBtn,'Position')));
+      
+      function buttonStopEstim(~,~)
+        setappdata(gcf,'data','stop');
+      end
+    end
     
+    function stop = sseplotpoint(x, ~, state, varargin)
+      stop = false;
+      
+      persistent plotx
+      if strcmpi(state, 'setup')
+        plotx = bar(x);
+        box off;
+        title('Current Point');
+        xlabel(sprintf('Varaibles: %d', length(x)));
+        set(plotx,'edgecolor','none')
+        set(gca,'xlim',[0,1 + length(x)])
+        
+        set(plotx,'Tag','sseplotx');
+        return
+      end
+      
+      if ~strcmpi(state, 'iter')
+        return
+      end
+      
+      set(plotx,'Ydata',x);
+    end
+    
+    function stop = sseplotvalue(~, optimValues, state, varargin)
+      stop = false;
+      if ~strcmpi(state, {'setup', 'init', 'iter', 'done'})
+        keyboard
+      end
+      
+      iter = varargin{1};
+      currentColor = [mod(iter, 2), 0, 1];
+      
+      persistent plotfval
+      if strcmpi(state, 'setup')
+        plotfval = plot(0, 0, 'kd','MarkerFaceColor',[1 0 1]);
+        box off
+        title(sprintf('Current Function Value:'));
+        xlabel('Iteration');
+        ylabel('Function value')
+        
+        set(plotfval, 'Tag','sseplotfval');
+        
+        plotfval.MarkerFaceColor = currentColor;
+        return
+      end
+      
+      iteration = optimValues.iteration;
+      fval = optimValues.fval;
+      
+      if strcmpi(state, 'init')
+        set(plotfval,'Xdata', iteration, 'Ydata', fval);
+        
+        plotfval.MarkerFaceColor = currentColor;
+        return
+      end
+      
+      newX = [get(plotfval,'Xdata') iteration];
+      newY = [get(plotfval,'Ydata') fval];
+      set(plotfval,'Xdata',newX, 'Ydata',newY);
+      set(get(gca,'Title'),'String',sprintf('Current Function Value: %g', fval));
+      
+      if strcmpi(state, 'done')
+        oldline = copyobj(plotfval, plotfval.Parent);
+        % Set markers off, color line based on solver
+        oldline.Marker = 'none';
+        oldline.LineStyle = '-';
+        oldline.Color = currentColor;
+        
+        set(plotfval,'Xdata',[], 'Ydata',[]);
+      end      
+    end
+  end
+    
+  methods (Hidden = true)
     %% EM Algorithm Helper functions
     function [beta, sigma] = restrictedOLS(y, X, V, J, F, G)
       % Restricted OLS regression
@@ -326,6 +497,5 @@ classdef StateSpaceEstimation < AbstractStateSpace
       sigma = (sigmaOLS + (betaOLS * F - G) / ...
         (F' / xxT * F) * (betaOLS * F - G)') / T_dim;
     end
-    
   end
 end
