4af5d4cd0dfa3a7ba2c525f2e87dc58bff5e72f4
David A. Kelley <dkelley@frbchi.org>
2017-06-01 13:18:01 -0500

    v1.5.1. Corrected bug in diffuse smoother. Improved estimation plotting.

--- build/compile_docs.m
+++ build/compile_docs.m
@@ -1,10 +1,17 @@
 function docs_status = compile_docs
 % Make the documentation for the MFSS toolbox
+% 
+% Compiling the documentation requires a conda environment named py27
+% with python 2.7 installed and the following packages installed from pip:
+%   sphinx
+%   sphinxcontrib-matlabdomain
+
+% David Kelley, 2017
 
 buildDir = fileparts(mfilename('fullpath'));
 docsDir = fullfile(fileparts(buildDir), 'docs');
 
-[sphinx_status, sphinx_msg] = system([docsDir '\make html']);
+[sphinx_status, sphinx_msg] = system(['activate py27 & ' docsDir '\make html']);
 if sphinx_status == 0 && contains(sphinx_msg, 'build succeeded')
   docs_status = 0;
 else
@@ -13,7 +20,6 @@ end
   
 % Copy the helptoc.xml file so Matlab knows what documentation we have:
 if docs_status == 0
-
   xmlSource = fullfile(docsDir, 'source', 'helptoc.xml');
   xmlDest = fullfile(docsDir, 'build', 'html', 'helptoc.xml');
   copyfile(xmlSource, xmlDest);
