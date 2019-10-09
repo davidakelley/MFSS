3dc169848d22c2d15f1380fa9fa8b64ebbbb09ea
David A. Kelley <dkelley@frbchi.org>
2019-10-09 16:01:13 -0500

    Add additional tests of V and J against the multivariate filter.

--- test/StateSpace_test.m
+++ test/StateSpace_test.m
@@ -93,6 +93,119 @@ classdef StateSpace_test < matlab.unittest.TestCase
       testCase.verifyEqual(ssV, V1, 'AbsTol', 5e-10);
       testCase.verifyEqual(ssJ, J, 'AbsTol', 5e-10);
     end
+    
+    function testVJ_VAR2_missing_long(testCase)
+      p = 3; 
+      lags = 2;
+      nT = 150;
+      
+      [y, ss] = mfvar_test.generateVAR(p, lags, nT);
+      y(1:100, 1) = nan;
+      
+      [~, sOut, fOut] = ss.smooth(y);      
+      ss = ss.setDefaultInitial();
+      [ssPrep, yPrep] = ss.prepareFilter(y, [], []);
+      sOut.N = cat(3, sOut.N, zeros(size(sOut.N, 1)));
+      [ssV, ssJ] = ssPrep.getErrorVariances(yPrep, fOut, sOut);
+      
+      [P, N, V, J] = StateSpace_test.kalmanMultiVariances(ss, lags, y);
+      
+      testCase.verifyEqual(fOut.P, P, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.N, N, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.V, ssV, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.V, V, 'AbsTol', 5e-10);
+      testCase.verifyEqual(ssJ, J, 'AbsTol', 5e-10);
+    end
+    
+    function testVJ_VAR2_missing_accum_long(testCase)
+      p = 3; 
+      lags = 2;
+      seed = 7;
+      
+      [y, ss] = mfvar_test.generateVAR(p, lags, 51, seed);
+      aggY = y;
+      aggY(:, 2) = Accumulator_test.aggregateY(y(:, 2), 3, 'avg');
+      
+      aggY(1:45,3) = nan;
+      
+      [~, sOut, fOut] = ss.smooth(aggY);      
+      ss = ss.setDefaultInitial();
+      [ssPrep, yPrep] = ss.prepareFilter(aggY, [], []);
+      sOut.N = cat(3, sOut.N, zeros(size(sOut.N, 1)));
+      [ssV, ssJ] = ssPrep.getErrorVariances(yPrep, fOut, sOut);
+      
+      [P, N, V, J] = StateSpace_test.kalmanMultiVariances(ss, lags, aggY);
+      
+      testCase.verifyEqual(fOut.P, P, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.N, N, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.V, ssV, 'AbsTol', 5e-10);
+      testCase.verifyEqual(sOut.V, V, 'AbsTol', 5e-10);
+      testCase.verifyEqual(ssJ, J, 'AbsTol', 5e-10);
+    end
+    
+  end
+  
+  methods(Static)
+    function [P, N, V, J, L] = kalmanMultiVariances(ss, nLags, y)
+      % Multivariate Kalman filter variance matrixes
+      pl = ss.p * nLags;
+      nT = size(y,1);
+      
+      P = zeros(pl, pl, nT+1);
+      F = zeros(ss.p, ss.p, nT);
+      L = zeros(pl, pl, nT);
+      
+      N = zeros(pl, pl, nT+1);
+      V = zeros(pl, pl, nT);
+      J = zeros(pl, pl, nT);
+      
+      wInd = ~isnan(y(1,:));
+      W = eye(ss.p);
+      W = W(wInd,:);
+
+      P(:,:,1) = ss.T * ss.P0 * ss.T' + ss.R * ss.Q * ss.R';
+      
+      WZ = W * ss.Z;
+      
+      F(wInd,wInd,1) = WZ * P(:,:,1) * WZ' + W * ss.H * W';
+      N(:,:,1) = WZ' / F(wInd,wInd,1) * WZ;
+      
+      V(:,:,1) = P(:,:,1) - P(:,:,1) * N(:,:,1) * P(:,:,1);
+      K = ss.T * P(:,:,1) * WZ' / F(wInd,wInd,1);
+      L(:,:,1) = ss.T - K * WZ;
+      J(:,:,1) = P(:,:,1) * L(:,:,1)';
+      
+      P(:,:,2) = ss.T * P(:,:,1) * L(:,:,1)' + ss.R * ss.Q * ss.R';
+      
+      for iT = 2:nT
+        wInd = ~isnan(y(iT,:));
+        W = eye(ss.p);
+        W = W(wInd,:);
+
+        WZ = W * ss.Z;
+
+        F(wInd,wInd,iT) = WZ * P(:,:,iT) * WZ' + W * ss.H * W';
+        
+        K = ss.T * P(:,:,iT) * WZ' / F(wInd,wInd,iT);
+        L(:,:,iT) = ss.T - K * WZ;
+        
+        P(:,:,iT+1) = ss.T * P(:,:,iT) * L(:,:,iT)' + ss.R * ss.Q * ss.R';
+      end
+      
+      % N(:,:,T+1) is zeros.
+      for iT = nT:-1:1
+        wInd = ~isnan(y(iT,:));
+        W = eye(ss.p);
+        W = W(wInd,:);
+
+        WZ = W * ss.Z;
+        
+        N(:,:,iT) = WZ' / F(wInd,wInd,iT) * WZ + L(:,:,iT)' * N(:,:,iT+1) * L(:,:,iT);
+        V(:,:,iT) = P(:,:,iT) - P(:,:,iT) * N(:,:,iT) * P(:,:,iT);
+        J(:,:,iT) = P(:,:,iT) * L(:,:,iT)' * (eye(pl) - N(:,:,iT+1) * P(:,:,iT+1));
+      end
+      
+    end
   end
   
 end
